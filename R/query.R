c175cc7ec009e7a83a7101e8f61d41ecf77922b8
Jinhwan Kim <hwanistic@gmail.com>
2021-10-06 16:02:51 +0900

    Update query.R

--- R/query.R
+++ R/query.R
@@ -1,34 +1,28 @@
-# install.packages("RMySQL")
-# print("여기까지는 되나")
-# library(RMySQL)
-# print("여기는?")
-
-# DB_NAME <- 
+install.packages("RMySQL")
+library(RMySQL)
 
 print(Sys.getenv())
 
-#con <- dbConnect(MySQL(),
-#    user = Sys.getenv("DB_USER"),
-#    password = Sys.getenv("DB_PW"),
-#    dbname = Sys.getenv("DB_NAME"),
-#    host = Sys.getenv("DB_HOST"),
-#    port = 3306
-#)
-#print("여기서  안되는건가?")
-#dbSendQuery(con, 'set character set "utf8"')
+con <- dbConnect(MySQL(),
+    user = Sys.getenv("DB_USER"),
+    password = Sys.getenv("DB_PW"),
+    dbname = Sys.getenv("DB_NAME"),
+    host = Sys.getenv("DB_HOST"),
+    port = 3306
+)
 
-# db<-dbSendQuery(con,SQL query)
+dbSendQuery(con, 'set character set "utf8"')
 
-#test_query <- dbGetQuery(
-#    con,
-#    "SELECT as2.title FROM application a
-#JOIN application_step_submission ass ON ass.applicationId = a.id
-#JOIN application_step as2 ON as2.id = ass.applicationStepId
-#WHERE a.productName = 'AI 부트캠프 7기'
-#GROUP BY as2.title "
-#)
+test_query <- dbGetQuery(
+    con,
+    "SELECT as2.title FROM application a
+JOIN application_step_submission ass ON ass.applicationId = a.id
+JOIN application_step as2 ON as2.id = ass.applicationStepId
+WHERE a.productName = 'AI 부트캠프 7기'
+GROUP BY as2.title "
+)
 
-#print(test_query)
-#data <- fetch(db, n = -1)
+print(test_query)
+data <- fetch(db, n = -1)
 
-# dbDisconnect(con)
+dbDisconnect(con)
