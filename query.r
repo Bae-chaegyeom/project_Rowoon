f6254884f3a2e05e04054876979bdebbe990e654
배채겸 <bae-chaegyeom@baechaegyeoms-MacBook-Pro.local>
2021-10-06 11:48:29 +0900

    start

---
+++ query.r
@@ -0,0 +1,25 @@
+install.packages("RMySQL")
+
+library(RMySQL)
+
+
+
+con <- dbConnect(MySQL(), user = ${{ secrets.DB_USER}}, password = ${{ secrets.DB_PW}}, dbname = ${{ secrets.DB_NAME}}, host = ${{ secrets.DB_HOST}})
+
+dbSendQuery(con, 'set character set "utf8"')
+
+# db<-dbSendQuery(con,SQL query)
+
+test_query <- dbGetQuery(
+    con,
+    "SELECT as2.title FROM application a
+JOIN application_step_submission ass ON ass.applicationId = a.id
+JOIN application_step as2 ON as2.id = ass.applicationStepId
+WHERE a.productName = 'AI 부트캠프 7기'
+GROUP BY as2.title "
+)
+
+test_query
+data <- fetch(db, n = -1)
+
+dbDisconnect(con)
