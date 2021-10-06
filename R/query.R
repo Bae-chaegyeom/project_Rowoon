e032b89f16f8a8669685669d964c7b5f30301878
배채겸 <bae-chaegyeom@baechaegyeoms-MacBook-Pro.local>
2021-10-06 18:12:50 +0900

    whyyyyy

--- R/query.R
+++ R/query.R
@@ -1,10 +1,6 @@
-
 install.packages("RMySQL")
-
 library(RMySQL)
 print(Sys.getenv())
-
-
 con <- dbConnect(MySQL(),
     user = Sys.getenv("DB_USERNAME"),
     password = Sys.getenv("DB_PASSWORD"),
@@ -12,10 +8,7 @@ con <- dbConnect(MySQL(),
     host = Sys.getenv("DB_HOST"),
     port = 3306
 )
-
-
 dbSendQuery(con, 'set character set "utf8"')
-
 test_query <- dbGetQuery(
     con,
     "SELECT as2.title FROM application a
@@ -24,9 +17,6 @@ JOIN application_step as2 ON as2.id = ass.applicationStepId
 WHERE a.productName = 'AI 부트캠프 7기'
 GROUP BY as2.title "
 )
-
+dbDisconnect(con)
 
 print(test_query)
-# data <- fetch(test_query, n = -1)
-
-dbDisconnect(con)
